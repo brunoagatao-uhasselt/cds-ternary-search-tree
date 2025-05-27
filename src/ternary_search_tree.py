23f65833ecffad5a365728c490be7b74205379b6
Bruno Agatao <brunoagatao@hotmail.com>
2025-05-27 22:09:18 +0100

    feat: add TreeNode class
    TreeNode instances shall represent traversal nodes in the ternary search tree.

---
+++ src/ternary_search_tree.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class TreeNodeChildren:
+    less_than: "TreeNode | None" = None
+    equals: "TreeNode | None" = None
+    larger_than: "TreeNode | None" = None
+
+
+class TreeNode:
+    def __init__(self, character: str):
+        self.character = character
+        self.terminates = False
+        self.children = TreeNodeChildren()
+
+    @property
+    def character(self) -> str:
+        return self._character
+
+    @character.setter
+    def character(self, character: str):
+        if len(character) > 1:  # allow empty string
+            raise ValueError("input must be a character string")
+        self._character = character
+
+    @property
+    def terminates(self) -> bool:
+        return self._terminates
+
+    @terminates.setter
+    def terminates(self, terminates: bool):
+        self._terminates = terminates
+
+    @property
+    def children(self) -> TreeNodeChildren:
+        return self._children
+
+    @children.setter
+    def children(self, children: TreeNodeChildren):
+        self._children = children
